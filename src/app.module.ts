831eef64b572f4463d718fb66e1be56578c765cf
David Jiménez <skyrroner@gmail.com>
2023-05-04 18:48:11 -0500

    fix: Add products to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsModule } from './controllers/products/products.module';
+import { ProductsController } from './controllers/products/products.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+        MongooseModule.forRoot('mongodb://localhost/ecommerce'),
+        ProductsModule,
+    ],
+  controllers: [AppController, ProductsController],
   providers: [AppService],
 })
 export class AppModule {}
