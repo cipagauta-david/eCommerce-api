f2b456d2a640da7e14763c751546a0d7818019c7
David Jiménez <skyrroner@gmail.com>
2023-05-04 21:17:23 -0500

    feat: Add mongodb conection to Raiway

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,12 @@ import { OrdersController } from './controllers/orders/orders.controller';
 
 @Module({
   imports: [
-        MongooseModule.forRoot('mongodb://127.0.0.1:27017/ecommerce'),
+        // MongooseModule.forRoot('mongodb://127.0.0.1:27017/ecommerce'),
+        MongooseModule.forRoot('mongodb://mongo:3z1V6cNoz9bDnQ6e0pgI@containers-us-west-55.railway.app:6551',{
+          user:'mongo',
+          pass:'3z1V6cNoz9bDnQ6e0pgI',
+          dbName:'ecommerce'
+        }),
         ProductsModule, OrdersModule
     ],
   controllers: [AppController, ProductsController, OrdersController],
